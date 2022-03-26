958926b01b213fa7daeae29c82fa7e35be1e5512
donghy1 <915110101@qq.com>
2022-03-26 20:42:43 +0800

    新增-添加express-async-handler 处理异步函数

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,23 +1,23 @@
-
-const getGoals = (req,res)=>{
-    res.status(200).send('Get goals')
-}
-const setGoals = (req,res)=>{
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('请传入数据')
-    }
-    res.status(200).send('Get goals')
-}
-const updateGoals = (req,res)=>{
-    res.status(200).send(`Get goals ${req.params.id}`)
-}
-const deleteGoals = (req,res)=>{
-    res.status(200).send(`Get goals ${req.params.id}`)
-}
+const asyncHandler = require("express-async-handler");
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).send("Get goals");
+});
+const setGoals = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("请传入数据");
+  }
+  res.status(200).send("Get goals");
+});
+const updateGoals = asyncHandler(async (req, res) => {
+  res.status(200).send(`Get goals ${req.params.id}`);
+});
+const deleteGoals = asyncHandler(async (req, res) => {
+  res.status(200).send(`Get goals ${req.params.id}`);
+});
 module.exports = {
-    getGoals,
-    setGoals,
-    updateGoals,
-    deleteGoals
-}
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+};
