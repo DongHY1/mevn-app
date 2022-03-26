f03cf80d16ec7ecbd1dd11ded1784e2d33ff81ed
donghy1 <915110101@qq.com>
2022-03-26 20:12:49 +0800

    新增-express服务器搭建,POSTMAN成功访问

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log(1123321)
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT||5000
+const app = express()
+app.get('/api/goals',(req,res)=>{
+    res.send('Get goals')
+})
+app.listen(port,()=>{
+    console.log('服务器在'+port+'端口监听')
+})
