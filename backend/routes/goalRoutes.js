5bb575e0e3054052ad8dd2a5c1377c49ae3c5cf0
donghy1 <915110101@qq.com>
2022-03-26 20:26:30 +0800

    修改-合并路由层请求

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const {getGoals,setGoals,updateGoals,deleteGoals} = require('../controller/goalController')
-router.get('/',getGoals)
-router.post('/',setGoals)
-router.put('/:id',updateGoals)
-router.delete('/:id',deleteGoals)
+router.route('/').get(getGoals).post(setGoals)
+router.route('/:id').delete(deleteGoals).put(updateGoals)
 module.exports = router
