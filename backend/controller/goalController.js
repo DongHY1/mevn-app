e96482908672f5d8acaee4d01426ba5d06a92ca7
donghy1 <915110101@qq.com>
2022-03-27 11:43:56 +0800

    新增-实现不同用户的修改、删除目标(updateGoals,setGoals)

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Goal = require("../models/goalModel.js");
 const User = require("../models/userModel.js");
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find({user:req.user.id});
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -13,17 +13,31 @@ const setGoals = asyncHandler(async (req, res) => {
   }
   const goal = await Goal.create({
     text: req.body.text,
-    user:req.user.id
+    user: req.user.id,
   });
   res.status(200).json(goal);
 });
 
 const updateGoals = asyncHandler(async (req, res) => {
+  // 获取路由id
   const goal = await Goal.findById(req.params.id);
   if (!goal) {
     res.status(400);
     throw new Error("没有找到id对应数据");
   }
+
+  // 用户校验
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(400);
+    throw new Error("没有找到用户");
+  }
+  // 确保登录用户 等于 目标用户
+  if (goal.user.toString() !== user.id) {
+    res.status(400);
+    throw new Error("用户未授权");
+  }
+
   const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
@@ -36,8 +50,20 @@ const deleteGoals = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("没有找到id对应数据");
   }
-  await goal.remove()
-  res.status(200).json({id:req.params.id});
+  // 用户校验
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(400);
+    throw new Error("没有找到用户");
+  }
+  // 确保登录用户 等于 目标用户
+  if (goal.user.toString() !== user.id) {
+    res.status(400);
+    throw new Error("用户未授权");
+  }
+
+  await goal.remove();
+  res.status(200).json({ id: req.params.id });
 });
 module.exports = {
   getGoals,
