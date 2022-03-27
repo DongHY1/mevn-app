d82fc557fb5adb1c08f2855b982241dd23c0d26d
donghy1 <915110101@qq.com>
2022-03-27 11:04:36 +0800

    新增-生成JWT验证

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -28,7 +28,8 @@ const registerUser = asyncHandler(async(req,res)=>{
           res.status(201).json({
              _id:user.id,
              name:user.name,
-             email:user.email
+             email:user.email,
+             token:generateToken(user._id)
           })
       }else{
         res.status(400)
@@ -43,7 +44,8 @@ const loginUser = asyncHandler(async(req,res)=>{
         res.json({
             _id:user.id,
             name:user.name,
-            email:user.email
+            email:user.email,
+            token:generateToken(user._id)
         })
     }else{
         res.status(400)
@@ -55,6 +57,11 @@ const loginUser = asyncHandler(async(req,res)=>{
 const getMe = asyncHandler(async(req,res)=>{
     res.status(200).json({message:'成功注册用户'});
 })
+const generateToken = (id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn:'30d'
+    })
+}
 module.exports = {
     registerUser,
     loginUser,
